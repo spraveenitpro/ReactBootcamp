771b3d1a8f61af4d260357bc751b22ea48843895
Praveen <spraveenitpro@gmail.com>
2020-03-31 23:18:14 -0700

    Week 2 exercises commit

---
+++ Week2/exercise-1/src/App.js
@@ -0,0 +1,41 @@
+import React from "react";
+import "./App.css";
+
+const Header = () => {
+  return (
+    <header id="myHeader">
+      <h2> Knowledge is of no value unless you put it into practice!</h2>
+    </header>
+  );
+};
+
+const Footer = () => {
+  return (
+    <footer id="myFooter">
+      <h3> Remember the end in the beginning</h3>
+    </footer>
+  );
+};
+
+const Content = () => {
+  return (
+    <main>
+      <h2>
+        “You may say I'm a dreamer, but I'm not the only one. I hope someday
+        you'll join us. And the world will live as one.” ― John Lennon
+      </h2>
+    </main>
+  );
+};
+
+function App() {
+  return (
+    <React.Fragment>
+      <Header />
+      <Content />
+      <Footer />
+    </React.Fragment>
+  );
+}
+
+export default App;
