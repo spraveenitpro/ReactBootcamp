b5371a65986b72b9fa082c11526a36dd6f8ba132
Praveen <spraveenitpro@gmail.com>
2020-04-17 23:12:23 -0700

    Completed the post reader assignment

--- Week4/post-reader/src/App.js
+++ Week4/post-reader/src/App.js
@@ -10,6 +10,40 @@ const Header = (props) => {
   );
 };
 
+const PostList = (props) => {
+  const { quote } = props;
+  const { posts } = props;
+
+  return (
+    <main>
+      <h2>{quote}</h2>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id} data-id={post.id}>
+            <a href={post.guid.rendered} onClick={props.handleClick}>
+              {post.title.rendered}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+};
+
+const ActualPost = (props) => {
+  for (const item of props.posts) {
+    if (props.postId == item.id) {
+      var content = item.content.rendered;
+    }
+  }
+  return (
+    <>
+      <h2> Article:</h2>
+      <p>{content}</p>
+    </>
+  );
+};
+
 const Footer = (props) => {
   const { quote } = props;
   return (
@@ -19,43 +53,45 @@ const Footer = (props) => {
   );
 };
 
-const Content = (props) => {
-  const { quote } = props;
-  return (
-    <main>
-      <h2>{quote}</h2>
-    </main>
-  );
-};
-
 class App extends React.Component {
   state = {
     posts: [],
+    isClicked: false,
+    selectedPostID: 0,
   };
+
+  handleClick = (e) => {
+    e.preventDefault();
+    const selectedPostID = e.target.parentNode.dataset.id;
+    this.setState({ isClicked: true, selectedPostID });
+    console.log(e.target);
+  };
+
   componentDidMount() {
-    fetch("https://woocommerce.mystagingwebsite.com/wp-json/wp/v2/posts")
+    let url = "https://ma.tt/wp-json/wp/v2/posts";
+    fetch(url)
       .then((response) => response.json())
       .then((posts) => {
         this.setState({ posts });
       });
   }
   render() {
-    const handleLinkClick = () => {
-      console.log("I clicked!!");
-    };
     return (
       <>
         <Header quote=" Knowledge is of no value unless you put it into practice!" />
-        <Content quote="Remember the end in the beginning" />
-        <main>
-          <ul>
-            {this.state.posts.map((post) => (
-              <li key={post.id}>
-                <a href={post.guid.rendered}>{post.title.rendered}</a>
-              </li>
-            ))}
-          </ul>
-        </main>
+        <PostList
+          quote="Remember the end in the beginning"
+          posts={this.state.posts}
+          handleClick={this.handleClick}
+        />
+        {this.state.isClicked ? (
+          <ActualPost
+            postId={this.state.selectedPostID}
+            posts={this.state.posts}
+          />
+        ) : (
+          ""
+        )}
         <Footer
           quote="You may say I'm a dreamer, but I'm not the only one. I hope someday
         you'll join us. And the world will live as one.” ― John Lennon"
